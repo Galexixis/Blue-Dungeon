66c5019322b15ff8eb7191fb90c4878a84035445
Galexixis <alex17fontain@gmail.com>
2026-06-27 21:25:16 +0200

    Ajout de l'event pause, et test d'une configuration de github

---
+++ core/src/main/java/blueDungeon/logic/common/eventSystem/gameEvent/PausedEvent.java
@@ -0,0 +1,29 @@
+package blueDungeon.logic.common.eventSystem.gameEvent;
+
+/**
+ * Evenement qui signal l'état de la mise en pause du jeu.
+ * Permet de savoir si l'evement vient d'une source automatique ou manuel.
+ * @author
+ */
+public class PausedEvent implements GameEvent {
+
+    private boolean pausedValue;
+    private boolean isManualAction;
+
+    /**
+     * @param pausedValue l'état de pause vers le quel basculer.
+     * @param isManualAction précise si l'evement vient d'une source manuel ou non.
+     */
+    public PausedEvent(boolean pausedValue, boolean isManualAction){
+        this.pausedValue = pausedValue;
+        this.isManualAction = isManualAction;
+    }
+
+    public boolean getPausedValue(){
+        return pausedValue;
+    }
+
+    public boolean isManualAction(){
+        return isManualAction;
+    }
+}
