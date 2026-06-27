7c4c3fc8815b53fbeb149963d60aad3ead2fee28
mimironi <ilyas.i.aliev@hotmail.com>
2026-06-27 21:32:22 +0200

    add engine abstract entity

---
+++ core/src/main/java/blueDungeon/engine/entity/Direction.java
@@ -0,0 +1,13 @@
+package blueDungeon.engine.entity;
+
+/**
+ * Represents the cardinal direction faced by an entity.
+ *
+ * @author mimron
+ */
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+}
